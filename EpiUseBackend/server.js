461de1fac1b3a8033d80fa10852b56135334184b
CheesecakeCoding <mynhardt1234@gmail.com>
2026-04-15 22:40:56 +0200

    Fixed endpoints for login

--- EpiUseBackend/server.js
+++ EpiUseBackend/server.js
@@ -1,29 +1,13 @@
 require("dotenv").config();
-
-const { Sequelize, DataTypes } = require("sequelize");
+const { Sequelize, DataTypes, Model } = require("sequelize");
 const crypto = require("crypto");
-const hash = crypto.createHash("sha256");
-/*var bodyParser = require('body-parser');
-const express = require("express");
-const app = express();
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));*/
 const express = require("express");
 const cors = require("cors");
 const bodyParser = require("body-parser");
-//const raw = require('body-parser/raw')
 const app = express();
-//app.use(express.json());
-app.use(bodyParser.urlencoded());
-app.use(cors());
-
-//app.use(bodyParser.urlencoded({extended:true}));
-
 const port = process.env.PORT || 3000;
-
 const sequelize = new Sequelize(process.env.DB_URL, {
   dialect: "postgres",
-  //logging: (err) => {console.log(err)},
   logging: false,
   dialectOptions: {
     ssl: {
@@ -32,27 +16,42 @@ const sequelize = new Sequelize(process.env.DB_URL, {
     },
   },
 });
+app.use(bodyParser.urlencoded());
+app.use(cors());
+
+class login_table extends Model {}
+login_table.init(
+  {
+    username: DataTypes.STRING,
+    password: DataTypes.STRING,
+    company: DataTypes.STRING,
+    firstname: DataTypes.STRING,
+    surname: DataTypes.STRING,
+  },
+  { sequelize, modelName: "login_table" },
+);
 
 sequelize
   .sync()
   .then(() => {
     console.log("Database connected");
+    //login_.sync();\
+    sequelize
+      .getQueryInterface()
+      .showAllSchemas()
+      .then((tableObj) => {
+        console.log("// Tables in database", "==========================");
+        console.log(tableObj);
+      })
+      .catch((err) => {
+        console.log("showAllSchemas ERROR", err);
+      });
   })
   .catch((err) => {
     console.log(err);
   });
 
-//   model schema
-const post = sequelize.define("post", {
-  title: {
-    type: DataTypes.STRING,
-    allowNull: false,
-  },
-  content: {
-    type: DataTypes.STRING,
-    allowNull: false,
-  },
-});
+//sequelize.model.login_table;
 
 /*
 app.get("/", (req, res) => {
@@ -67,7 +66,7 @@ app.get("/createTables", async (req, res) => {
     var [result, metadata] = await sequelize.query(` 
             create table login_table (
               username VARCHAR(150) PRIMARY KEY,
-              pass VARCHAR(255) not null,
+              password VARCHAR(255) not null,
               company VARCHAR(255),
               name VARCHAR(50) not null,
               surname VARCHAR(60) not null
@@ -83,7 +82,7 @@ app.get("/createTables", async (req, res) => {
 app.get("/insertTable", async (req, res) => {
   try {
     var [result, metadata] = await sequelize.query(` 
-            insert into login_table (username, pass, company, name, surname)
+            insert into login_table (username, pass, company, firstname, surname)
             values ('mynhardt1234@gmail.com', '123', 'CompanyName', 'Kevin', 'Mynhardt');
         `);
     res.json({ Status: "ok", res: `${result}` });
@@ -96,7 +95,7 @@ app.get("/insertTable", async (req, res) => {
 app.get("/TableCheck", async (req, res) => {
   try {
     var [result, metadata] = await sequelize.query(` 
-            select * from login_table;
+            select * from login_table
         `);
     console.log(result);
 
@@ -109,14 +108,8 @@ app.get("/TableCheck", async (req, res) => {
 app.get("/DropTables", async (req, res) => {
   try {
     var [result, metadata] = await sequelize.query(` 
-            DO $$ 
-            DECLARE 
-                r RECORD;
-            BEGIN 
-                FOR r IN (SELECT tablename FROM pg_tables WHERE schemaname = 'public') LOOP
-                    EXECUTE 'DROP TABLE IF EXISTS ' || quote_ident(r.tablename) || ' CASCADE';
-                END LOOP; 
-            END $$;
+          DROP TABLE IF EXISTS public.login_tables;
+          DROP TABLE IF EXISTS public.login_table;
         `);
     console.log(result);
 
@@ -148,26 +141,22 @@ app.get("/get-posts", async (req, res) => {
 app.post("/createUser", async (req, res) => {
   try {
     var { username, password, name, surname } = req.body;
-    password = getHashAndSalt(password, name, surname);
-    //xxx Do exsitance check
-    /*var [count, rows] = await sequelize.findAndCountAll({where: {username}});
-      if (result.row[0].username != username)*/
-    var [result, metadata] = await sequelize
-      .query(
-        ` 
-          insert into login_table (username, pass, name, surname)
-          values (:user, :pass, :n, :s);
-      `,
-        {
-          replacements: {
-            user: username,
-            pass: password,
-            n: name,
-            s: surname,
-          },
-          type: sequelize.QueryTypes.INSERT,
-        },
-      )
+    password = getHashAndSalt(username, password);
+    var existingUser = await getUserInformation(username);
+    if (existingUser.length != 0) {
+      res.status(409).json({
+        message: `Account using the email specified already exists`,
+        login: false,
+      });
+      return;
+    }
+    var result = await login_table
+      .create({
+        username: `${username}`,
+        password: `${password}`,
+        firstname: `${name}`,
+        surname: `${surname}`,
+      })
       .then(
         res
           .status(201)
@@ -179,26 +168,33 @@ app.post("/createUser", async (req, res) => {
 });
 
 app.post("/login", async (req, res) => {
+  var token = createToken();
   try {
     var { username, password } = req.body;
+
     password = getHashAndSalt(username, password);
-    //xxx --where username = ${username}
-    var result = await sequelize.query(` 
-      select password, company from login_table 
-    `);
-    //xxx Do check for username existence
+    var result = await getUserInformation(username);
+    console.log(`val: ${JSON.stringify(result)}`);
+    if (result.hasOwnProperty("err")) {
+      res.status(500).json({
+        message: `Failed login: error with db`,
+        login: false,
+        err: result.err,
+      });
+      return;
+    }
     if (result.length == 0) {
       res
-        .status(200)
+        .status(404)
         .json({ message: `Failed login: username not found`, login: false });
+      return;
     }
+    console.log(`Return val: ${result[0].password}`);
     if (result[0].password != password) {
-      res
-        .status(200)
-        .json({
-          message: `Failed login: username and password mismatch`,
-          login: false,
-        });
+      res.status(200).json({
+        message: `Failed login: username and password mismatch`,
+        login: false,
+      });
     }
   } catch (err) {
     res.status(500).json({ error: err, path: "login" });
@@ -220,18 +216,21 @@ app.get("/endpoint", async(req, res) => {
 app.get("/checkdb", async (req, res) => {});
 
 function getHashAndSalt(username, password) {
-  hash.update(`${username}${password}${username}`);
-  return hash.digest("hex");
+  var hash_new = crypto.createHash("sha256");
+  hash_new.update(`${username}${password}`);
+  return hash_new.digest("hex");
 }
 
-function getUserInformation(username) {
+async function getUserInformation(username) {
   try {
-    var result = sequelize.query(` 
-      select * from login_table where username = ${username};
-    `);
-    return result[0];
+    var result = await login_table.findAll({
+      where: { username: `${username}` },
+    });
+    //console.log(`length: ${result.length}`);
+    //result = JSON.stringify(result);
+    return JSON.stringify(result);
   } catch (err) {
-    res.status(500).json({ error: err, path: "login" });
+    return { error: err, path: "getUserInformation" };
   }
 }
 
@@ -239,6 +238,6 @@ function createToken() {
   //xxx Add session based tokens for better security
 }
 
-app.listen(port, () => {
+app.listen(port, async () => {
   console.log(`App listening at http://localhost:${port}`);
 });
