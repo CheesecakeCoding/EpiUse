816be2ba6ada8eae4f9e4095e8421fab79d023a3
CheesecakeCoding <mynhardt1234@gmail.com>
2026-04-15 18:25:19 +0200

    Fixing deployment issues

--- EpiUseFrontend/src/App.jsx
+++ EpiUseFrontend/src/App.jsx
@@ -1,12 +1,6 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "./assets/vite.svg";
-import heroImg from "./assets/hero.png";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.css";
-import ListGroup from "./components/ListGroup";
 import LoginPanel from "./components/LoginPanel";
-import Register from "./components/Register";
 
 function App() {
   //const [count, setCount] = useState(0)
